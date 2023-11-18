7366e4819d6f120ae7fa92e4597df075f6a57e36
Rendi <rendicrew96@gmail.com>
2023-11-18 16:04:53 +0700

    fix feat: HomeGateReport

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,11 +1,13 @@
 import 'package:port_pass_app/core/services/injection_container.dart';
+import 'package:port_pass_app/src/activity_management/presentation/views/detail_activity_screen.dart';
 import 'package:port_pass_app/src/auth/presentation/bloc/auth_bloc.dart';
 import 'package:port_pass_app/src/auth/presentation/views/sign_in_screen.dart';
 import 'package:port_pass_app/src/dashboard/presentation/views/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:port_pass_app/src/gate-report/presentation/views/home_gate_report_screen.dart';
 import '../../src/auth/presentation/views/splash_screen.dart';
-import '../../src/gate-report/presentation/views/home-gate-report-screen.dart';
+
 import '../common/views/page_under_construction.dart';
 
 part 'router.main.dart';

9a3e09fd4e361aee6d00461369576df022f147de
JabalnurIT <jabalnur.it@gmail.com>
2023-11-19 01:42:27 +0700

    update feat: container_card

--- lib/core/common/widgets/container_card.dart
+++ lib/core/common/widgets/container_card.dart
@@ -24,38 +24,51 @@ class ContainerCard extends StatefulWidget {
 class _ContainerCardState extends State<ContainerCard> {
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: widget.backgroundColor,
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(20),
-              ),
-            ),
-            padding: const EdgeInsets.all(10),
-            margin: widget.headerHeight != null
-                ? EdgeInsets.only(top: widget.headerHeight!)
-                : null,
-            height: MediaQuery.of(context).size.height *
-                (widget.mediaHeight ?? 0.8),
-            child: Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: ListView(
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                children: widget.children,
-              ),
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: widget.backgroundColor,
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  margin: widget.headerHeight != null
+                      ? EdgeInsets.only(top: widget.headerHeight!)
+                      : null,
+                  height: MediaQuery.of(context).size.height *
+                      (widget.mediaHeight ?? 0.8),
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: ListView(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      children: widget.children,
+                    ),
+                  ),
+                ),
+                if (widget.header != null)
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: widget.header,
+                    ),
+                  ),
+              ],
             ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: widget.header,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }

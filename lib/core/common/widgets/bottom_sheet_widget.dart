cc75273c1eadf7dff6fd58d35ae8c238fa8555a8
Rendi <rendicrew96@gmail.com>
2023-11-30 01:21:59 +0700

    add feat: calendarWidget

--- lib/core/common/widgets/bottom_sheet_widget.dart
+++ lib/core/common/widgets/bottom_sheet_widget.dart
@@ -2,22 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:port_pass_app/core/res/colours.dart';
 
 class BottomSheetWidget extends StatelessWidget {
-  const BottomSheetWidget({super.key, required this.child, this.buttons});
+  const BottomSheetWidget({super.key, required this.child, this.buttons,this.buttonsBottomPosition, this.height,});
 
   final Widget child;
   final List<Widget>? buttons;
+  final double? buttonsBottomPosition;
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       decoration: const BoxDecoration(
-        color: Colors.white,
+        color: Colours.secondaryColour,
         borderRadius: BorderRadius.vertical(
           top: Radius.circular(20),
         ),
       ),
-      height: MediaQuery.of(context).size.height,
+      height: MediaQuery.of(context).size.height * (height ?? 0.6),
       child: Stack(
         children: [
           Positioned(
@@ -37,7 +39,7 @@ class BottomSheetWidget extends StatelessWidget {
           ),
           child,
           Positioned(
-            bottom: 62,
+            bottom: buttonsBottomPosition ?? 62,
             left: 0,
             right: 0,
             child: Align(

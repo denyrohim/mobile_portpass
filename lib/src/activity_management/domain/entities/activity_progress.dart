57efe665ab901b12713efe12c9e9ffa4aebed844
ALMAGEST12 <williamzefanya115@gmail.com>
2023-12-06 01:02:00 +0700

    update feat : tracking activity screen

--- lib/src/activity_management/domain/entities/activity_progress.dart
+++ lib/src/activity_management/domain/entities/activity_progress.dart
@@ -4,13 +4,15 @@ class ActivityProgress extends Equatable {
   final String name;
   final String date;
   final String time;
+  final String status;
 
   const ActivityProgress({
     required this.name,
     required this.date,
     required this.time,
+    required this.status,
   });
 
   @override
-  List<Object?> get props => [name, date, time];
+  List<Object?> get props => [name, date, time, status];
 }

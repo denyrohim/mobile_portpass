8b91594f8f23edc1dd8383ebe55a48d49e138bc1
JabalnurIT <jabalnur.it@gmail.com>
2023-12-31 03:03:34 +0800

    change minor

--- lib/core/utils/constanst.dart
+++ lib/core/utils/constanst.dart
@@ -4,5 +4,5 @@ const kDefaultAvatar =
     'https://images.freeimages.com/fic/images/icons/573/must_have/256/user.png';
 
 const kBaseUrl = "https://portpass.iwandp.my.id";
+// const kBaseUrl = "https://demo.javadwipa.com/portpassv2/public";
 const kHost = "$kBaseUrl/api";
-// const kHost = "portpass.iwandp.my.id/api";

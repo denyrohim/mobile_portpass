1547fefd949053ed004f7ffe54e8a1d7154e700c
Deny Rohim <denyrohim@gmail.com>
2024-05-20 08:24:15 +0700

    Update constanst.dart

--- lib/core/utils/constanst.dart
+++ lib/core/utils/constanst.dart
@@ -4,5 +4,6 @@ const kDefaultAvatar =
     'https://images.freeimages.com/fic/images/icons/573/must_have/256/user.png';
 
 // const kBaseUrl = "https://portpass.iwandp.my.id";
-const kBaseUrl = "https://demo.javadwipa.com/portpassv2/public";
+//const kBaseUrl = "https://demo.javadwipa.com/portpassv2/public";
+const kBaseUrl = "https://portpass.petrokimia-gresik.com";
 const kHost = "$kBaseUrl/api";
